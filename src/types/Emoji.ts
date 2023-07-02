868a271a816d27d5bfd30778d17b0366e07df83c
Murilo <mbalves1@outlook.com>
2023-07-01 22:55:50 -0300

    [feat]- included interface

---
+++ src/types/Emoji.ts
@@ -0,0 +1,18 @@
+
+type Emoji =
+  | "confused"
+  | "cool"
+  | "crying"
+  | "grinning-squinting"
+  | "happy"
+  | "heart-eyes"
+  | "laugh"
+  | "sad"
+  | "shocked"
+  | "smiling"
+  | "smiling-with-eyes"
+  | "surprised"
+  | "wink"
+  | "worried";
+
+export default Emoji;
